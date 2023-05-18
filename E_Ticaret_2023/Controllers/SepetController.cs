4fb5cf6a2b72f9f2bbdf61ae4bdc87e4cf824fd3
cengizelif <cenelif@gmail.com>
2023-05-18 13:46:29 +0300

    sepet güncelle

--- E_Ticaret_2023/Controllers/SepetController.cs
+++ E_Ticaret_2023/Controllers/SepetController.cs
@@ -5,20 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using E_Ticaret_2023.Models;
+
 
 namespace E_Ticaret_2023.Controllers
 {
     public class SepetController : Controller
     {
-        // GET: Sepet
+        E_Ticaret_2023Entities db = new E_Ticaret_2023Entities();
         public ActionResult Index()
         {
-            return View();
+            string userId = User.Identity.GetUserId();
+            var sepet = db.Sepet.Where(x => x.KullaniciId == userId);
+            return View(sepet.ToList());
         }
         public ActionResult SepeteEkle(int UrunId, int adet) 
-        {
-            E_Ticaret_2023Entities db = new E_Ticaret_2023Entities();
+        {      
 
             string userId = User.Identity.GetUserId();
             Urunler urun = db.Urunler.Find(UrunId);
@@ -46,5 +47,34 @@ namespace E_Ticaret_2023.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public ActionResult SepetGuncelle(int id,int adet)
+        {
+            Sepet sepet = db.Sepet.Find(id);
+            if(sepet==null) 
+            {
+                return HttpNotFound();
+            }
+
+            Urunler urun = db.Urunler.Find(sepet.UrunId);
+
+            sepet.Adet = adet;
+            sepet.ToplamTutar=sepet.Adet* urun.UrunFiyati;  
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SepetSil(int id)
+        {
+            Sepet sepet=db.Sepet.Find(id);  
+            if(sepet!=null)
+            {
+                db.Sepet.Remove(sepet);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
